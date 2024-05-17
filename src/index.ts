150e98c6836020fce65a0a3b493417330f986b08
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-05-17 13:40:41 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Response, Request } from 'express'
+
+const app = express ();
+const port = 3000;
+
+
+app.get ('/', (req: Request,res: Response)=>{
+    res.send ("Hello World");
+})
+
+app.listen (port, ()=>{
+    console.log (`App is listening on: http://localhost:${port}`)
+})
